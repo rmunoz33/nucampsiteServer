1f5742f6e6e9224a2f630bcb378c06f19da23870
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2020-02-27 13:18:39 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '1235-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '574773956578403',
+        clientSecret: '2c7afca1e3aaa385a912c1a460f1471c'
+    }
 }
